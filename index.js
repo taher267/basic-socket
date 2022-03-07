909441feeb4f52a250460788f6a415276ec95a7c
ABU TAHER <abutaher267@gmail.com>
2022-03-07 17:48:19 +0600

    according to namaspace broadcast

--- index.js
+++ index.js
@@ -11,13 +11,29 @@ app.get('/', (req, res) => {
 
 expressServer.listen(3001, () => {
     console.log(`Alhamdu lillah, server is listining on port 3001...`);
+});
+
+let buyNsp = io.of('/buy');
+buyNsp.on('connection', socket => {
+    buyNsp.emit('buyEvent', "This is buy envet");
+});
+let sellNsp = io.of('/sell');
+sellNsp.on('connection', socket => {
+    sellNsp.emit('sellEvent', "This is sell event")
 })
 
 
-io.on('connection', socket => {
-    console.log("Alhamdu lillah, soccet connected");
-    io.sockets.emit('myBroadcast', "ASSALMU ALAIKUM");
-});
+
+/**
+ * Basic Broadcasting
+ */
+// io.on('connection', socket => {
+//     console.log("Alhamdu lillah, soccet connected");
+//     io.sockets.emit('myBroadcast', "ASSALMU ALAIKUM");
+// });
+
+
+//basic data read / write
 // io.on('connection', (socket) => {
 //     console.log("New user Connected");
 //     socket.on('name', (msg) => { console.log(msg); })
